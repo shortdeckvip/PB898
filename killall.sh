bbf4e106f044d4ef56e058f7e6cc895cc6335f75
magicminglee <magicminglee@gmail.com>
2021-12-17 13:25:00 +0800

    update

--- killall.sh
+++ killall.sh
@@ -1,7 +1,7 @@
 tag="$1"
 
 if [ "$tag" == "all" ]; then
-	../tools/redis/redis-cli --no-auth-warning -u "redis://Ch1904@redis.poker.game:5000/0" DEL ONLINE
+	/usr/local/bin/redis-cli --no-auth-warning -u "redis://Ch1904@redis.poker.game:5000/0" DEL ONLINE
 	kill `ps x | grep "Server" | grep -v "grep" |  awk '{print $1}' | sort -r`
 elif [ "$tag" == "game" ]; then
 	kill `ps x | grep "LuaServer" | grep -v "grep" | awk '{print $1}' | sort -r`
