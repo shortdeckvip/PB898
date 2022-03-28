ec9cccff5ab6277391efc1d34a218b9452e438cf
magicminglee <magicminglee@gmail.com>
2022-03-28 14:35:51 +0800

    增加磁盘监控脚本

--- write.sh
+++ write.sh
@@ -4,10 +4,17 @@ function onCtrlC () {
         exit 0
 }
 
+requrl=https://web.wgnice.com/api/dc/bot.php
+keyword="/data"
+used=`df -h | grep -w $keyword | awk '{print $5}' | sed 's/%//g'`
 while true
 do
-        { echo -ne "HTTP/1.0 200 OK\r\n\r\n"; } | nc -vl 39002 > .pushnclogs
-        git pull origin main
-        git commit . -m "update at `date +%m-%d-%H%M%S`"
-        git push origin main
+        if [ ${used} -gt 80 ]; then
+                msg="disk has used ${used} and is grater than 80%"
+                curl -G --data-urlencode "cli=0" --data-urlencode "sign=56d70103d4e6100d33cee37b7175ad98" --data-urlencode "msg=$msg" $requrl
+                echo ${msg}
+        else
+                echo "used ${used}"
+        fi
+        sleep 5
 done
